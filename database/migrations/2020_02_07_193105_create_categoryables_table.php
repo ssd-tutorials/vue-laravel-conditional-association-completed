fdb6722ba8d1edc48cae0afe0d36a0301ad64eab
Sebastian Sulinski <info@ssdtutorials.com>
2020-02-07 21:06:37 +0000

    Renamed migration (typo)

--- database/migrations/2020_02_07_193105_create_categoryables_table.php
+++ database/migrations/2020_02_07_193105_create_categoryables_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CrateCategoryablesTable extends Migration
+class CreateCategoryablesTable extends Migration
 {
     /**
      * Run the migrations.
